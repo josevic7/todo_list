c9a78de8a0f5a575bbd2b3e524ad595804f204a7
josevic7 <156964696+josevic7@users.noreply.github.com>
2025-04-21 19:14:43 -0500

    profile page customization and final CSS styling

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path 
-from . import views 
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDeleteView, CustomLoginView, RegisterPage, ProfilePage
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
+
+
+
+print("DEBUG: TaskCreate is", TaskCreate)
 
 urlpatterns = [
-    path('', views.taskList, name='tasks'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
+    path('profile/', ProfilePage.as_view(), name='profile'), 
 ]
